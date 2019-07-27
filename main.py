0d07e6cfca89b33888a9039508ddc9f1af763f28
bijonguha <iitbguha@gmail.com>
2019-07-27 05:42:06 +0530

    1. removed logging in from for loop 2. introduced support for outlook, live and hotmail

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ Created on Sat Jul 27 02:34:01 2019
 from source.utilities import send_mail
 import pandas as pd
 import os
-import time
 
 def main(sender_email, password, receiver_file, subject_file, salutation,\
-         body_plain, body_html, html_flag):
+         body_plain, body_html, html_flag, pause):
     
     contacts = pd.read_excel(os.path.join('data',receiver_file))    
     subject = open(os.path.join('data',subject_file), "r").read()
@@ -21,16 +20,9 @@ def main(sender_email, password, receiver_file, subject_file, salutation,\
     if(html_flag == 'True'):
         body_html = open(os.path.join('data',body_html), "r").read()
         html = True
-    
-    
-    for i in range(len(contacts)):
-        receiver_email = contacts.loc[i,'email']
-        receiver_name = contacts.loc[i,'name']
-        print('Sending email to %s' %receiver_email)
-        send_mail(sender_email, password, receiver_email, receiver_name, subject,\
-              salutation, body_plain, body_html, html)
-        time.sleep(6)
-        print('email sent !')
+
+    send_mail(sender_email, password, contacts, subject,\
+          salutation, body_plain, body_html, html, pause)
         
 if __name__ == "__main__":
     import argparse
@@ -42,7 +34,8 @@ if __name__ == "__main__":
     parser.add_argument('-sal','--salutation',default='salutation.txt', help='Salutation for beginning Message')
     parser.add_argument('-bod','--body', default='body_plain.txt', help='Plain body of Message')
     parser.add_argument('-bod_h','--body_html', default='body_html.txt', help='html body of Message')
-    parser.add_argument('--html', default = 'False', help = 'True if msg is html format')
+    parser.add_argument('-html','--html', default = 'False', help = 'True if msg is html format')
+    parser.add_argument('-p','--pause', default = 6, type= int, help = 'Pause timing between each email')
     
     args = parser.parse_args()
     
@@ -54,9 +47,10 @@ if __name__ == "__main__":
     body_html = args.body_html
     html_flag = args.html
     salutation = args.salutation
+    pause = args.pause
     
     main(sender_email, password, receiver_file, subject_file, salutation,\
-         body_plain, body_html, html_flag)
+         body_plain, body_html, html_flag, pause)
 
     
     
--- source/utilities.py
+++ source/utilities.py
@@ -1,44 +1,61 @@
-import smtplib, ssl
+import smtplib, ssl, time
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
-def send_mail(sender_email, password, receiver_email, receiver_name, subject,\
-              salutation, body_plain, body_html, html):
-    
-    sender_email = sender_email#"fab.alwaysme@gmail.com"  # Enter sender gmail address
-    receiver_email = receiver_email#"iitbguha@gmail.com"  # Enter receiver address
-    password = password#'bg06071994' #password of sender's gmail
+def send_mail(sender_email, password, contacts, subject,\
+              salutation, body_plain, body_html, html, pause):
 
-    message = MIMEMultipart("alternative")
-    message["Subject"] = subject #subject of email
-    message["From"] = sender_email
-    message["To"] = receiver_email
-
-    # Create the plain-text and HTML version of your message
-    text = salutation+' '+receiver_name+',\n\n'+body_plain
-    part1 = MIMEText(text, "plain")
-    message.attach(part1)
-    
-    if(html == True):
-        html = salutation+' '+receiver_name+',\n\n'+body_html
-        part2 = MIMEText(html, "html")
-        message.attach(part2)
-    
-    try: 
-        # Create secure connection with server and send email
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-            server.login(sender_email, password)
-            server.sendmail(
-                sender_email, receiver_email, message.as_string()
-            )
-        return 1
-    
-    except smtplib.SMTPException:
-        print('SMTPAuthenticationError: %s' % 'Your account name or password is\
-              incorrect, please try again using the correct stuff')
+    if(sender_email.split('@')[1] == 'gmail.com'):
+        smtp_server = "smtp.gmail.com"
+    elif(sender_email.split('@')[1] == 'outlook.com' or 'live.com' or 'hotmail.com'):
+        smtp_server = "smtp-mail.outlook.com"
+    else: 
+        print('Email id domain unknown')
         return -1
+    
+    port = 587
+    context = ssl.create_default_context()
+    
+    try:
+        server = smtplib.SMTP(smtp_server,port)
+        server.ehlo() # Can be omitted
+        server.starttls(context=context) # Secure the connection
+        server.ehlo() # Can be omitted
+        server.login(sender_email, password)    
+        print('Authentication successful')
+        for i in range(len(contacts)):
+            tic = time.time()
+            
+            receiver_email = contacts.loc[i,'email']# Enter receiver address
+            receiver_name = contacts.loc[i,'name']
+            print('Sending email to %s' %receiver_email)
+            
+            message = MIMEMultipart("alternative")
+            message["Subject"] = subject #subject of email
+            message["From"] = sender_email
+            message["To"] = receiver_email
+
+            # Create the plain-text and HTML version of your message
+            text = salutation+' '+receiver_name+',\n\n'+body_plain
+            part1 = MIMEText(text, "plain")
+            message.attach(part1)
+            
+            if(html == True):
+                html = salutation+' '+receiver_name+',\n\n'+body_html
+                part2 = MIMEText(html, "html")
+                message.attach(part2)
+            
+    
+            server.sendmail(sender_email, receiver_email, message.as_string())
+            time.sleep(pause)
+            print('email sent !',time.time()-tic)
+            
+    except Exception as e:
+        # Print any error messages to stdout
+        print(e)
+    finally:
+        server.quit() 
 
 
     
