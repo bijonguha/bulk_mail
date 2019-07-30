1b0e0a77f24cf9ce5ac4f5b2e222d279238a4acd
bijonguha <iitbguha@gmail.com>
2019-07-31 00:12:39 +0530

    saving log files

--- bulk_mail/source/utilities.py
+++ bulk_mail/source/utilities.py
@@ -1,10 +1,12 @@
 import smtplib, ssl, time
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+import pandas as pd
 
 def send_mail(sender_email, password, contacts, salutation_html, body_html, \
                                           signature_html, pause, domain):
     
+    res_logs = []
     #Check for email id domain for connecting to right smtp_server
     if (domain == 'auto'):
         #If domain is auto, email id should have ending gmail.com or outlook.com
@@ -41,11 +43,16 @@ def send_mail(sender_email, password, contacts, salutation_html, body_html, \
         print('Authentication successful, Logged in')
 
         for i in range(len(contacts)):
+
+            res = [] #collecting results
+
             tic = time.time()
             
             receiver_email = contacts.loc[i,'email']# Enter receiver address
             receiver_name = contacts.loc[i,'name']
             print('Sending email to %s' %receiver_email)
+            res.append(receiver_email)
+            res.append(receiver_name)
 
             message = MIMEMultipart("alternative")
             message["Subject"] = contacts.loc[i,'subject'] #subject of email
@@ -64,17 +71,22 @@ def send_mail(sender_email, password, contacts, salutation_html, body_html, \
                 print('Unable to send mail or Bad email id encountered at %d, Please check' %i)
                 continue
             
+            res.append('success')
+            res_logs.append(res)
+
             time.sleep(pause)
             print('email sent !',time.time()-tic)
 
         server.quit()
-        return
+        df_logs = pd.DataFrame(res_logs)
+        df_logs.columns = ['email', 'name', 'status']
+        return df_logs,1
            
     except Exception as e:
         # Print any error messages to stdout
         print(e)
         print('Unable to Connect with Server Or Authentication issue -> Please try again with correct username password')
-        return
+        return 0,0
 
 
 
